463ec1819066dfbde58da1728e201ef925608adb
Museop <museopkim0214@gmail.com>
2023-01-10 22:26:22 +0900

    solve 11659

--- 03-data-structure/ragdoll/11659.py
+++ 03-data-structure/ragdoll/11659.py
@@ -1,13 +1,31 @@
-counts = input()
-numbers = [int(number) for number in input().split()]
-start, end = [int(range) - 1 for range in input().split()]
+# ============================================================================
+# 시간 초과 발생
+#
+# number_count, calculation_count = [int(count) for count in input().split()]
+# numbers = [int(number) for number in input().split()]
+#
+# interval_sum = [0]
+# sum = 0
+# for number in numbers:
+#     sum += number
+#     interval_sum.append(sum)
+#
+# for index in range(calculation_count):
+#     start, end = [int(interval) for interval in input().split()]
+#     print(interval_sum[end] - interval_sum[start - 1])
+# ============================================================================
+import sys
 
-interval_sum = []
-sum = 0
-for number in numbers:
-    sum += number
-    interval_sum.append(sum)
+input = sys.stdin.readline
+suNo, quizNo = map(int, input().split())
+numbers = list(map(int, input().split()))
+prefix_sum = [0]
+temp = 0
 
-print(interval_sum)
+for i in numbers:
+    temp = temp + i
+    prefix_sum.append(temp)
 
-print(interval_sum[end] - interval_sum[start - 1])
+for i in range(quizNo):
+    s, e = map(int, input().split())
+    print(prefix_sum[e] - prefix_sum[s-1])
