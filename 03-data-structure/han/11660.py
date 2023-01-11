04bbf9db8d2f47288b6407f150ef7dcdf95247e5
102092 <quaint3@gmail.com>
2023-01-11 21:34:37 +0900

    Clean up format

--- 03-data-structure/han/11660.py
+++ 03-data-structure/han/11660.py
@@ -5,26 +5,25 @@ import sys
 input = sys.stdin.readline
 
 
-# def solve():
-nm = list(input().split())
-n = int(nm[0])
-m = int(nm[1])
-A = [[0] * (n + 1)]
-D = [[0] * (n + 1) for _ in range(n + 1)]  # _ -> dummy variable
+def solve():
+    nm = list(input().split())
+    n = int(nm[0])
+    m = int(nm[1])
+    A = [[0] * (n + 1)]
+    D = [[0] * (n + 1) for _ in range(n + 1)]  # _ -> dummy variable
 
-# 원본 리스트 저장
-for i in range(n):
-    row = [0] + [int(x) for x in input().split()]
-    A.append(row)
+    # 원본 리스트 저장
+    for i in range(n):
+        row = [0] + [int(x) for x in input().split()]
+        A.append(row)
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        # D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j], 이걸 생각해낼수 있었을까?
-        D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j]
 
-for i in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
-    print(D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1])
+    for i in range(m):
+        x1, y1, x2, y2 = map(int, input().split())
+        print(D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1])
 
 
 if __name__ == '__main__':
